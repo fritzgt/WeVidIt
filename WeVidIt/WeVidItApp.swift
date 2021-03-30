9813bc3d4cbb5e76b7ee43c8500a04c0590b50ab
fritzgt <fritzgt@icloud.com>
2021-03-30 23:20:52 +0300

    Initial Commit

---
+++ WeVidIt/WeVidItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeVidItApp.swift
+//  WeVidIt
+//
+//  Created by FGT MAC on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct WeVidItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
